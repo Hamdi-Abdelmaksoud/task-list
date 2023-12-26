39d1d7216b894303e50f6442532bd4d435bc95ba
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2023-12-26 13:26:56 +0100

    creer un lyaout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,9 @@
-<h1>
-    List of tasks
-</h1>
+@extends('layouts.app')
+@section('title','The list of tasks')
+@section('content')
 <div>
     @foreach ($tasks as $task )
     <div><a href="{{route('tasks.show',['id'=>$task->id])}}">{{$task->title}}</a></div>
     @endforeach
-
 </div>
+@endsection
