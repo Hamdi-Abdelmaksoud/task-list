81db25f5ef983356de7232c9ba581bc2bff0c4af
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-01-05 13:19:49 +0100

    creat fake data tasks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(10)->create();
-//we use command php artisan db:seed to add fake data
+        //we use command php artisan db:seed to add fake data
+        \App\Models\Task::factory(20)->create();
+ // php artisan migrate:refresh -seed if we want to refresh data
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
