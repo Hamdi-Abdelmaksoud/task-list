f2a6caef8dc36fb946a2d1d93da5432f7d55275f
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-01-05 13:25:20 +0100

    add unverified fake data to fake users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         //we use command php artisan db:seed to add fake data
+        \App\Models\User::factory(2)->unverified()->create();//ad two models unverified
         \App\Models\Task::factory(20)->create();
  // php artisan migrate:refresh -seed if we want to refresh data
         // \App\Models\User::factory()->create([
