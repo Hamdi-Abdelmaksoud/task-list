a73d4a3a07042f7900c4512d6664ad013124730b
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-01-15 18:46:13 +0100

    srtyling forms

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -16,38 +16,38 @@
 @method('PUT')
 
 @endisset
-<div>
+<div class="mb-4">
     <label for="title">
         Title
     </label>
-<input type="text" name="title" id="title" value="{{$task->title ?? old('title')}}"/>
+<input type="text" name="title" id="title" value="{{$task->title ?? old('title')}}" @class(['border-red-500'=>$errors->has('title')])/>
 @error('title')
 <p class="error-message">{{$message}}</p>
 
 @enderror
 </div>
-<div>
+<div class="mb-4">
     <label for="description">
         Description
     </label>
-<textarea name="description"  id="description" rows="5">{{$task->description ?? old('description')}}</textarea>
+<textarea name="description"  id="description" rows="5"  @class(['border-red-500'=>$errors->has('description')])>{{$task->description ?? old('description')}}</textarea>
     @error('description')
     <p class="error-message">{{$message}}</p>
 
     @enderror
 </div>
-<div>
+<div class="mb-4">
     <label for="long_description">
         Long Description
     </label>
-<textarea name="long_description"  id="long_description" rows="10">{{$task->long_description ?? old('long_description')}}</textarea>
+<textarea name="long_description"  @class(['border-red-500'=>$errors->has('long_description')]) id="long_description" rows="10">{{$task->long_description ?? old('long_description')}}</textarea>
 @error('long_description')
 <p class="error-message">{{$message}}</p>
 
 @enderror
 </div>
 <div>
-    <button type="submit">@isset($task)
+    <button class="btn"  type="submit">@isset($task)
         Update Task
         @else
         Add Task
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,11 +9,18 @@
 {{-- blade-formatter-disable --}}
 <style type="text/tailwindcss">
 .btn{
-    @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50 text-slate-700
+    @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700/10  bg-blue-500 hover:bg-blue-400 text-slate-700
 }
 .link{
     @apply font-medium text-gray-700 underline decoration-pink-500
 }
+label{
+    @apply block uppercase text-slate-700 mb-2
+}
+input,textarea{
+    @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none
+}
+
 </style>
 {{-- blade-formatter-enable --}}
     @yield('styles')
