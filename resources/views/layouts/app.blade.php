c46185354759997190ed09671721329e57c0ef32
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-01-16 12:18:59 +0100

    customize flash messages(alpine.js)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 10 Task List App</title>
     <script src="https://cdn.tailwindcss.com"></script>
-{{-- blade-formatter-disable --}}
+    <script src="//unpkg.com/alpinejs" defer></script>
+    {{-- blade-formatter-disable --}}
 <style type="text/tailwindcss">
 .btn{
     @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700/10  bg-blue-500 hover:bg-blue-400 text-slate-700
@@ -27,11 +28,24 @@ input,textarea{
 </head>
 <body class="container mx-auto mt-10 max-w-lg">
 <h1 class="text-2xl mb-4">@yield('title')</h1>{{-- section title --}}
-<div>
+<div x-data="{flash:true}">{{-- init flash varaible--}}
     @if (session()->has('success'))
-    <div>{{session('success')}}</div>
 
-    @endif
+
+    <div x-show="flash"{{--condition--}}
+    class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700 " role="alert" role="alert">
+    <strong class="font-bold">Success!</strong>
+    <div>flash message</div>
+    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+        {{-- on click flash=false --}}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+        stroke-width="1.5" @click="flash = false"
+        stroke="currentColor" class="h-6 w-6 cursor-pointer">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+    </svg>
+</span>
+</div>
+@endif
 @yield('content'){{-- section contenu  --}}
 </div>
 </body>
